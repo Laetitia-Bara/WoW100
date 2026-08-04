46ae078a32a107dd5db73c4b14e4cdcfb2190548
Laëtitia BARA <bara.laetitia@gmail.com>
2026-08-04 16:14:37 +0200

    fix ios

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\baral\Documents\DEV\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\WoW100"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=D:\WoW100\macos\Flutter\ephemeral\Packages\.packages\FlutterFramework"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.7"
+export "FLUTTER_BUILD_NUMBER=14"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
